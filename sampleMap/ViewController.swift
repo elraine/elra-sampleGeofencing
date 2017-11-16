75e18dcf0318100826320622a104aef807bb9723
elraine <laevatinn@gmail.com>
2017-11-16 17:56:05 +0100

    Initial Commit

---
+++ sampleMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleMap
+//
+//  Created by Alexis Chan on 16/11/2017.
+//  Copyright © 2017 achan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
